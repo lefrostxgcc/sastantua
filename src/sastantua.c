b8e9ccc79210527c32d3cfd3d479bf1bff3b1fd8
Chip <chipz68q@gmail.com>
2018-11-06 14:53:59 +0500

    Ready for Rozalek v2

--- src/sastantua.c
+++ src/sastantua.c
@@ -2,41 +2,41 @@
 
 void	sastantua(int size)
 {
-	int	i;
-	int width;
+	int	line;
+	int line_width;
 	int curr_size;
-	int size_top_i;
+	int size_last_line;
 	int door_size;
-	int	h;
+	int	total_height;
 	int max_width;
 	int need_print_key;
 	int print_door_size;
 	int blank_count;
 	int center_size;
 
-	h = calc_total_height(size);
+	total_height = calc_total_height(size);
 	max_width = calc_size_width(size);
-	i = 0;
-	width = 3;
+	line = 0;
+	line_width = 3;
 	curr_size = 1;
 	door_size = 1;
-	size_top_i = calc_size_height(curr_size);
-	while (i < h)
+	size_last_line = calc_size_height(curr_size);
+	while (line < total_height)
 	{
-		blank_count = (max_width - width) / 2;
-		center_size = width - 2;
-		print_door_size = calc_door_size_on_line(i, h, door_size);
-		need_print_key = is_key_on_line(i, h, door_size);
+		blank_count = (max_width - line_width) / 2;
+		center_size = line_width - 2;
+		print_door_size = calc_door_size_on_line(line, total_height, door_size);
+		need_print_key = is_key_on_line(line, total_height, door_size);
 		print_line(blank_count, center_size, print_door_size, need_print_key);
-		i++;
-		if (i >= size_top_i)
+		line++;
+		if (line >= size_last_line)
 		{
 			curr_size++;
 			if (curr_size % 2 == 1)
 				door_size += 2;
-			width += calc_size_height(curr_size) / 2 * 2;
-			size_top_i += calc_size_height(curr_size);
+			line_width += calc_size_height(curr_size) / 2 * 2;
+			size_last_line += calc_size_height(curr_size);
 		}
-		width += 2;
+		line_width += 2;
 	}
 }
